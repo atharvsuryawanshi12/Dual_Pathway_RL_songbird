379c4c182c557bd930d71c53563e4ef3bcb92287
Atharv Suryawanshi <133670200+AtharvSuryawanshi@users.noreply.github.com>
2024-06-17 15:41:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import numpy as np
 import os 
+import matplotlib.pyplot as plt
 save_dir = "plots"
-TEST_NOS = 5
-seeds = np.random.randint(0, 100, TEST_NOS)
+TEST_NOS = 20
+seeds = np.random.randint(0, 500, TEST_NOS)
+seeds = np.sort(seeds)
 print("Seeds: ", seeds)
 
 from module import build_and_run
 from tqdm import tqdm
-sum = 0
+
+annealing_returns = []
+non_annealing_returns = []
 
 for filename in os.listdir(save_dir):
     if filename.endswith(".png") or filename.endswith(".jpg"):
@@ -15,13 +19,20 @@ for filename in os.listdir(save_dir):
         
 for i in tqdm(range(TEST_NOS)):
     seed = seeds[i]
-    sum += build_and_run(seed, True, False)
-    tqdm.write(f"Times won:{sum}")
-print(f"success rate = {sum/TEST_NOS} ")
+    returns = build_and_run(seed, True, True)
+    annealing_returns.append(returns)
+    returns = build_and_run(seed, False, True)
+    non_annealing_returns.append(returns)
+print(f"Annealing = {annealing_returns} ")
+print(f"Non annealing = {non_annealing_returns}")
 
-sum = 0
-for i in tqdm(range(TEST_NOS)):
-    seed = seeds[i]
-    sum += build_and_run(seed, False, False)
-    tqdm.write(f"Times won:{sum}")
-print(f"success rate = {sum/TEST_NOS} ")
+
+fig, ax = plt.subplots(1,1) 
+ax.plot(annealing_returns, label='Annealing')
+ax.plot(non_annealing_returns, label='Non-Annealing')
+plt.xticks(np.arange(0, TEST_NOS, step=1), labels=seeds)
+ax.legend()
+ax.set_xlabel('Seeds')
+ax.set_ylabel('Returns')
+fig.suptitle('Results', fontsize=20)
+plt.show()
